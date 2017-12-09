c018e97d9533b222d0401a0605c903a2c038b2d4
george-tifrea <georgetifrea@gmail.com>
2017-12-09 14:52:25 +0200

    Added UserValidator

---
+++ OnlineTestManager/Service/Validators/UserValidator.cs
@@ -0,0 +1,16 @@
+﻿using Data.Core.Domain;
+using FluentValidation;
+
+namespace Service.Validators
+{
+    public class UserValidator: AbstractValidator<User>
+    {
+        public UserValidator()
+        {
+            RuleFor(x => x.FirstName).NotEmpty().Length(5, 30);
+            RuleFor(x => x.LastName).NotEmpty().Length(5, 30);
+            RuleFor(x => x.Email).NotEmpty().EmailAddress();
+            RuleFor(x => x.PasswordHash).NotEmpty();
+        }
+    }
+}
