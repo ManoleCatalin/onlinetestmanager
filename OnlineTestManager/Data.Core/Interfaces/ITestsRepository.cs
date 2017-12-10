3cd13318df0f956e5328ea0f21b181685ededae9
george-tifrea <georgetifrea@gmail.com>
2017-12-10 17:25:55 +0200

    Added TestsRepository and unit tests

---
+++ OnlineTestManager/Data.Core/Interfaces/ITestsRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Data.Core.Domain;
+
+namespace Data.Core.Interfaces
+{
+    public interface ITestsRepository
+    {
+        Task<List<Test>> GetTestsAsync();
+        Task<Test> GetTestByIdAsync(Guid id);
+        Task<Test> InsertTestAsync(Test renameMeWithLowerCaseTest);
+        Task<bool> UpdateTestAsync(Test renameMeWithLowerCaseTest);
+        Task<bool> DeleteTestAsync(Guid id);
+    }
+}
