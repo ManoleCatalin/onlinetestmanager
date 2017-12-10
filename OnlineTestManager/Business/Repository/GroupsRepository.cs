5c71c790e4d383a00de920ba7bb9bd34ebd5d6f8
Bordeianu Razvan <bordeianu_razvan_2006@yahoo.com>
2017-12-10 16:41:41 +0200

    Added crud for Group class

---
+++ OnlineTestManager/Business/Repository/GroupsRepository.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Data.Core.Domain;
+using Data.Core.Interfaces;
+using Data.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace Business.Repository
+{
+    public class GroupsRepository : IGroupsRepository
+    {
+        private readonly DatabaseContext _context;
+
+        public GroupsRepository(DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Group>> GetGroupsAsync()
+        {
+            return await _context.Groups.ToListAsync();
+        }
+
+        public async Task<Group> GetGroupByIdAsync(Guid id)
+        {
+            return await _context.Groups.FirstOrDefaultAsync(g => g.Id == id);
+        }
+
+        public async Task<Group> InsertGroupAsync(Group caseGroup)
+        {
+            _context.Groups.Add(caseGroup);
+            await _context.SaveChangesAsync();
+            return caseGroup;
+        }
+
+        public async Task<bool> UpdateGroupAsync(Group caseGroup)
+        {
+            _context.Groups.Update(caseGroup);
+            return await _context.SaveChangesAsync() > 0;
+        }
+
+        public async Task<bool> DeleteGroupAsync(Guid id)
+        {
+            _context.Groups.Remove(_context.Groups.FirstOrDefault(g => g.Id == id));
+            return await _context.SaveChangesAsync() > 0;
+        }
+    }
+}
---
+++ OnlineTestManager/TestManagementIntegrationTests/Base/GroupsRepositoryTests.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Business.Repository;
+using Data.Core.Domain;
+using FluentAssertions;
+using Xunit;
+
+namespace TestManagementIntegrationTests.Base
+{
+    public class GroupsRepositoryTests : BaseIntegrationTest
+    {
+        [Fact]
+        public void Given_Groups_When_GetGroupsAsyncsIsCalled_Then_ShouldReturnZeroGroups()
+        {
+            RunOnDatabase(context => {
+                // ARRANGE 
+                var groupRepository = new GroupsRepository(context);
+
+                // ACT
+                var groups = groupRepository.GetGroupsAsync();
+                var counter = groups.Result.Count;
+                // ASSERT
+                counter.Should().Be(0);
+            });
+        }
+
+        [Fact]
+        public void Given_Group_When_NewGroupIsAdded_Then_ShouldHaveOneGroupInDatabase()
+        {
+            RunOnDatabase(context => {
+                // ARRANGE 
+
+
+                context.UserTypes.Add(UserType.Create("student"));
+                context.SaveChanges();
+
+                var userType = context.UserTypes.ToList().FirstOrDefault();
+                var user = User.Create(
+
+
+                    "User1",
+                    "User last name",
+                    "test@test.ro",
+                    "parola",
+                    userType.Id
+
+                );
+                context.Users.Add(user);
+                context.SaveChanges();
+
+                var userId = context.Users.ToList().FirstOrDefault();
+
+                var groupRepository = new GroupsRepository(context);
+                var group = Group.Create(
+                    
+                    "A2",
+                    "grupa din anul III",
+                    userId.Id
+                
+                    );
+                var groupInserted = groupRepository.InsertGroupAsync(group).Result;
+                // ACT
+                var result = groupRepository.GetGroupByIdAsync(groupInserted.Id);
+                // ASSERT
+                result.Should().NotBe(null);
+            });
+
+        }
+
+        [Fact]
+        public void Given_Group_When_UpdateGroupAsync_Then_ShouldBeTrue()
+        {
+            RunOnDatabase(context => {
+                // ARRANGE 
+
+
+                context.UserTypes.Add(UserType.Create("student"));
+                context.SaveChanges();
+
+                var userType = context.UserTypes.ToList().FirstOrDefault();
+                var user = User.Create(
+
+
+                    "User1",
+                    "User last name",
+                    "test@test.ro",
+                    "parola",
+                    userType.Id
+
+                );
+                context.Users.Add(user);
+                context.SaveChanges();
+
+                var userId = context.Users.ToList().FirstOrDefault();
+
+                var groupRepository = new GroupsRepository(context);
+                var group = Group.Create(
+
+                    "A2",
+                    "grupa din anul III",
+                    userId.Id
+
+                );
+
+                context.Add(group);
+                context.SaveChanges();
+
+                group.Update("A3", "grupa din anul I", userId.Id);
+
+
+                // ACT
+                var result = groupRepository.UpdateGroupAsync(group);
+                // ASSERT
+                result.Result.Should().Be(true);
+            });
+
+        }
+
+        [Fact]
+        public void Given_Group_When_DeleteGroupAsync_Then_ShouldBeTrue()
+        {
+            RunOnDatabase(context =>
+            {
+                // ARRANGE 
+
+
+                context.UserTypes.Add(UserType.Create("student"));
+                context.SaveChanges();
+
+                var userType = context.UserTypes.ToList().FirstOrDefault();
+                var user = User.Create(
+
+
+                    "User1",
+                    "User last name",
+                    "test@test.ro",
+                    "parola",
+                    userType.Id
+
+                );
+                context.Users.Add(user);
+                context.SaveChanges();
+
+                var userId = context.Users.ToList().FirstOrDefault();
+
+                var groupRepository = new GroupsRepository(context);
+                var group = Group.Create(
+
+                    "A2",
+                    "grupa din anul III",
+                    userId.Id
+
+                );
+
+                context.Add(group);
+                context.SaveChanges();
+        
+                // ACT
+                var result = groupRepository.DeleteGroupAsync(group.Id);
+                // ASSERT
+                result.Result.Should().Be(true);
+            });
+
+        }
+    }
+}
+
