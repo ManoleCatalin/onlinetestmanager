8a7f8d8d26f660f7694fb6918ca45fe8f7066ff3
george-tifrea <georgetifrea@gmail.com>
2017-12-10 15:59:11 +0200

    Added TestTypesRepository and unit tests for it

---
+++ OnlineTestManager/Data.Core/Interfaces/ITestTypesRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Data.Core.Domain;
+
+namespace Data.Core.Interfaces
+{
+    public interface ITestTypesRepository
+    {
+        Task<List<TestType>> GetTestTypesAsync();
+        Task<TestType> GetTestTypeByIdAsync(Guid id);
+        Task<TestType> InsertTestTypeAsync(TestType testType);
+        Task<bool> UpdateTestTypeAsync(TestType testType);
+        Task<bool> DeleteTestTypeAsync(Guid id);
+    }
+}
---
+++ OnlineTestManager/TestManagementIntegrationTests/Base/TestTypesRepositoryTests.cs
@@ -0,0 +1,78 @@
+﻿using Business.Repository;
+using Data.Core.Domain;
+using FluentAssertions;
+using Xunit;
+namespace TestManagementIntegrationTests.Base
+{
+    public class TestTypesRepositoryTests : BaseIntegrationTest
+    {
+        [Fact]
+        public void Given_TestTypes_When_GetTestTypesAsyncsIsCalled_Then_ShouldReturnZeroTestTypes()
+        {
+            RunOnDatabase(context =>
+            {
+                // ARRANGE 
+                var testTypesRepository = new TestTypesRepository(context);
+
+                // ACT
+                var testTypes = testTypesRepository.GetTestTypesAsync();
+                var counter = testTypes.Result.Count;
+                // ASSERT
+                counter.Should().Be(0);
+            });
+        }
+
+        [Fact]
+        public void Given_TestType_When_NewTestTypeIsAdded_Then_ShouldHaveOneTestTypeInDatabase()
+        {
+            RunOnDatabase(context =>
+            {
+                // ARRANGE 
+                var testTypeRepository = new TestTypesRepository(context);
+                var testType = TestType.Create("grila");
+                var testTypeInserted = testTypeRepository.InsertTestTypeAsync(testType).Result;
+                // ACT
+                var result = testTypeRepository.GetTestTypeByIdAsync(testTypeInserted.Id);
+                // ASSERT
+                result.Should().NotBe(null);
+            });
+        }
+
+        [Fact]
+        public void Given_TestType_When_UpdateTestTypeAsync_Then_ShouldBeTrue()
+        {
+            RunOnDatabase(context => {
+                // ARRANGE 
+                var testTypeRepository = new TestTypesRepository(context);
+                var testType = TestType.Create("grila");
+                context.TestTypes.Add(testType);
+                context.SaveChanges();
+                // ACT
+                testType.Update("normal");
+                var result = testTypeRepository.UpdateTestTypeAsync(testType);
+                // ASSERT
+                
+                result.Result.Should().Be(true);
+            });
+
+        }
+
+        [Fact]
+        public void Given_TestType_When_DeleteTestTypeAsync_Then_ShouldBeTrue()
+        {
+            RunOnDatabase(context => {
+                // ARRANGE 
+                var testTypeRepository = new TestTypesRepository(context);
+                var testType = TestType.Create("grila");
+                context.TestTypes.Add(testType);
+                context.SaveChanges();
+                // ACT
+                testType.Update("normal");
+                var result = testTypeRepository.DeleteTestTypeAsync(testType.Id);
+                // ASSERT
+                result.Result.Should().Be(true);
+            });
+
+        }
+    }
+}
