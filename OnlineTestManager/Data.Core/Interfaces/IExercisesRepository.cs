655cc4f89505d00a1e1217dcd64ddbf9dfd4a8a2
FlorinCojocariu <cojocariu.cojocariu9@gmail.com>
2017-12-10 17:05:09 +0200

    Implemented CRUT operations for Exercise class

---
+++ OnlineTestManager/Data.Core/Interfaces/IExercisesRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Data.Core.Domain;
+
+namespace Data.Core.Interfaces
+{
+    public interface IExercisesRepository
+    {
+        Task<List<Exercise>> GetExerciseAsync();
+        Task<Exercise> GetExerciseByIdAsync(Guid id);
+        Task<Exercise> InsertExerciseAsync(Exercise exercise);
+        Task<bool> UpdateExerciseAsync(Exercise exercise);
+        Task<bool> DeleteExerciseAsync(Guid id);
+    }
+}
