79be6ad664b327dbc5f44c94c8cee20addc4a6eb
EmaStrainu <strainuema@gmail.com>
2017-12-12 22:21:47 +0200

    Added IGenericRepository.

---
+++ OnlineTestManager/Data.Core/Interfaces/IGenericRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Data.Core.Domain;
+
+namespace Data.Core.Interfaces
+{
+    public interface IGenericRepository<T> where T : BaseEntity
+    {
+        Task<bool> DeleteAsync(Guid id);
+        Task<List<T>> GetAllAsync();
+        Task<T> GetByIdAsync(Guid id);
+        Task<T> InsertAsync(T entity);
+        Task<bool> UpdateAsync(T entity);
+    }
+}
