aef724342495855fd456faeb1e03856cd42bd19a
CatalinManole <manolecatalinconstantin@gmail.com>
2017-12-31 18:18:19 +0200

    Added missing file TestTypesConstants.cs

---
+++ OnlineTestManager/CrosscuttingConcerns/TestTypesConstants.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace Constants
+{
+    public static class TestTypesConstants
+    {
+        public const string MultipleChoiceTestType = "MultipleChoice";
+
+        private static readonly string[] s = { MultipleChoiceTestType };
+
+        public static List<string> GetTestTypeNames()
+        {
+            return new List<string>(TestTypeNames);
+        }
+    }
+}
