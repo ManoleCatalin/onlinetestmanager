58fa700add1810123a432b3b06a673e9ee232ffe
EmaStrainu <strainuema@gmail.com>
2018-01-13 15:59:36 +0200

    Moved validation for unique add of a student to a group.

--- OnlineTestManager/OTM/Validators/GroupValidators/AddStudentToGroupValidator.cs
+++ OnlineTestManager/OTM/Validators/GroupValidators/AddStudentToGroupValidator.cs
@@ -10,21 +10,33 @@ namespace OTM.Validators.GroupValidators
     public class AddStudentToGroupValidator : AbstractValidator<AddStudentToGroupViewModel>
     {
         private readonly IUsersRepository _usersRepository;
+        
         public AddStudentToGroupValidator(IUsersRepository users, IUserContext user, IGroupsRepository groups)
         {
             _usersRepository = users;
             RuleFor(x => x.StudentName)
                 .NotEmpty().WithMessage(string.Format(Consts.FieldEmptyMessage, "Student Name"))
                 .MaximumLength(Consts.MaxLength)
-                .WithMessage(string.Format(Consts.FieldMaximumLengthMessage, "Student Name", Consts.MaxLength))
-                .Custom((x, context) =>
+                .WithMessage(string.Format(Consts.FieldMaximumLengthMessage, "Student Name", Consts.MaxLength));
+               
+            RuleFor(x=>x).Custom((x, context) =>
+            {
+                var student = _usersRepository.GetStudentsByNamePrefixAsync(x.StudentName).Result;
+                if (student.Count == 0)
                 {
-                    var student = _usersRepository.GetStudentsByNamePrefixAsync(x).Result;
-                    if (student.Count == 0)
+                    context.AddFailure("StudentName", "Student name is not valid");
+                }
+                var group = groups.GetByIdAsync(x.GroupId).Result;
+                foreach (var userGroup in group.UserGroups)
+                {
+                    if (userGroup.User.UserName.Equals(x.StudentName))
                     {
-                        context.AddFailure("StudentName", "Student name is not valid");
+                        context.AddFailure("StudentName", "This user is already in this group");
+
                     }
-                });
+                }
+
+            });
             RuleFor(x => x.GroupId)
                 .NotEmpty().WithMessage(string.Format(Consts.FieldEmptyMessage, "Group Id"))
                 .Custom((x, context) =>
@@ -44,6 +56,7 @@ namespace OTM.Validators.GroupValidators
                         }
                     }
                 });
+           
         }
     }
 }
