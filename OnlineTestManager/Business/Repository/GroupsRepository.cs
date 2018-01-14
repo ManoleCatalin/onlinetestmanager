3db62ec6749fdd95d3527d5d619186a1cf687e82
EmaStrainu <strainuema@gmail.com>
2018-01-14 14:12:57 +0200

    Solved bug with adding student that has been deleted from a group. There is a bug because a update a UserGroup and this method has to be asynch.

--- OnlineTestManager/Business/Repository/GroupsRepository.cs
+++ OnlineTestManager/Business/Repository/GroupsRepository.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.Linq;
 using System.Threading.Tasks;
 using Business.Repository.Base;
@@ -13,42 +14,65 @@ namespace Business.Repository
 {
     public class GroupsRepository : GenericRepository<Group>, IGroupsRepository
     {
+     
         public GroupsRepository(DatabaseContext context) : base(context)
         {
-
+        
         }
         public override async Task<Group> GetByIdAsync(Guid id)
         {
-            return await _entities
+             Group groups = await _entities
+                .Where(b => b.IsDeleted == false)
                 .Include(s => s.UserGroups)
                 .ThenInclude(x => x.User)
                 .FirstOrDefaultAsync(x => x.Id == id );
+
+            List<UserGroup> listGroup = groups.UserGroups.ToList();
+            listGroup.RemoveAll(x => x.IsDeleted);
+            groups.UserGroups = listGroup;
+
+            return groups;
         }
+     
 
         public async Task<bool> InsertStudentAsync(Guid groupId, Guid studentId)
         {
-            var user = await _context.Users.FirstOrDefaultAsync(u => u.Id == studentId);
+            
+            User user = await _context.Users.FirstOrDefaultAsync(u => u.Id == studentId);
             if (user.Id != studentId)
             {
                 return false;
             }
 
-            var studentRole = await _context.Roles.FirstOrDefaultAsync(role => role.Name.Equals(RoleConstants.StudentRoleName));
+            Role studentRole = await _context.Roles.FirstOrDefaultAsync(role => role.Name.Equals(RoleConstants.StudentRoleName));
 
-            var isStudent = await _context.UserRoles.FirstOrDefaultAsync( ur => ur.UserId == studentId && ur.RoleId == studentRole.Id);
+            UserRole isStudent = await _context.UserRoles.FirstOrDefaultAsync( ur => ur.UserId == studentId && ur.RoleId == studentRole.Id);
 
             if (isStudent.UserId != studentId)
             {
                 return false;
             }
-
-            _context.UserGroups.Add(UserGroup.Create(studentId, groupId));
-            return await _context.SaveChangesAsync() > 0;
+            var group = await _context.UserGroups.FirstOrDefaultAsync((g => g.GroupId.Equals(groupId)  && g.UserId.Equals(studentId)));
+            if (@group != null && @group.IsDeleted)
+            {
+                group.IsDeleted = false;
+                _context.UserGroups.Update(group);
+                return await _context.SaveChangesAsync() > 0;
+            }
+            else
+            {
+                _context.UserGroups.Add(UserGroup.Create(studentId, groupId));
+                return await _context.SaveChangesAsync() > 0;
+            }
+            
+           
+            
         }
+       
 
         public async Task<bool> DeleteStudentAsync(Guid groupId, Guid studentId)
         {
-            var entity=_context.UserGroups
+            UserGroup entity=_context.UserGroups
                 .FirstOrDefault(ug => ug.UserId == studentId && ug.GroupId == groupId);
             if (entity == null) return await _context.SaveChangesAsync() > 0;
             entity.IsDeleted = true;
